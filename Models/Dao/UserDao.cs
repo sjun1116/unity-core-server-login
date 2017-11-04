cfba86054085839e75c0e533b56d9d449a18cfee
sjun1116 <30386423+sjun1116@users.noreply.github.com>
2017-11-05 03:51:14 +0900

    Update UserDao.cs

--- Models/Dao/UserDao.cs
+++ Models/Dao/UserDao.cs
@@ -65,7 +65,10 @@ namespace DotnetCoreServer.Models
                         access_token, diamond, health, defense, damage,
                         speed, health_level, defense_level, 
                         damage_level, speed_level,
-                        level, experience
+                        level, experience , name ,mana,
+                        money, magicdamege, scenenumber,
+                        str , int, statpoint, xpos, ypos, zpos,
+                        critical ,criticaldamege
                     FROM tb_user 
                     WHERE user_id = {0}",
                      UserID);
@@ -98,7 +101,20 @@ namespace DotnetCoreServer.Models
                             user.SpeedLevel = reader.GetInt32(15);
                             user.Level = reader.GetInt32(16);
                             user.Experience = reader.GetInt32(17);
-                            
+
+                            user.Name = reader.GetString(18);
+                            user.Mana = reader.GetInt32(19);
+                            user.Money = reader.GetInt32(20);
+                            user.MagicDamege = reader.GetInt32(21);
+                            user.SceneNumber = reader.GetInt32(22);
+                            user.Str = reader.GetInt32(23);
+                            user.Int = reader.GetInt32(24);
+                            user.StatPoint = reader.GetInt32(25);
+                            user.xPos = reader.GetInt32(26);
+                            user.yPos = reader.GetInt32(27);
+                            user.zPos = reader.GetInt32(28);
+                            user.Critical = reader.GetInt32(29);
+                            user.CriticalDamege = reader.GetInt32(30);                            
                         }
                     }
                 }
@@ -138,12 +154,16 @@ namespace DotnetCoreServer.Models
                     UPDATE tb_user SET 
                         health = {0}, defense = {1}, damage = {2}, speed = {3},
                         health_level = {4}, defense_level = {5}, damage_level = {6}, speed_level = {7},
-                        diamond = {8}, point = {9}
-                    WHERE user_id = {10}
+                        diamond = {8}, point = {9}, name = {10} ,mana = {11}, money = {12}, magicdamege = {13},
+                        scenenumber = {14}, str= {15} , int, statpoint = {16}, xpos = {17}, ypos = {18}, 
+                        zpos = {19}, critical = {20} ,criticaldamege = {21}  
+                    WHERE user_id = {22}
                     ",
                     user.Health, user.Defense, user.Damage, user.Speed,
                     user.HealthLevel, user.DefenseLevel, user.DamageLevel, user.SpeedLevel,
-                    user.Diamond, user.Point, user.UserID
+                    user.Diamond, user.Point, user.Name, user.Mana, user.Money,  user.MagicDamege, 
+                    user.SceneNumber, user.Str, user.Int, user.StatPoint, user.xPos, user.yPos, 
+                    user.zPos, user.Critical, user.CriticalDamege, user.UserID
                      );
 
                 Console.WriteLine(query);
